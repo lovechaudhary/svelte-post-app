935e6f1b1ad8f389449da9ed913451a2caf916b5
Love Chaudhary <lovechaudhary17@gmail.com>
2019-07-28 19:40:56 -0400

    write CRUD application functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
-	export let name;
+  import "../node_modules/materialize-css/dist/css/materialize.min.css";
+  import "../node_modules/materialize-css/dist/js/materialize.min.js";
+
+  import { Router, Route } from "svelte-routing";
+  import Navbar from "./layout/Navbar.svelte";
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+
 </style>
 
-<h1>Hello {name}!</h1>
+<Router>
+  <Navbar />
+  <div class="container">
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+</Router>
